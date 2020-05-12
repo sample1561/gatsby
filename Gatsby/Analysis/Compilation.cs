a99d007c03a7022848c81486008dd0454b2526f3
Sidharth Bhandari <sample1561@gmail.com>
2020-05-12 19:22:02 +0530

    Refactor the code to allow adding it as a project dependency

---
+++ Gatsby/Analysis/Compilation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Gatsby.Analysis.Semantic;
+using Gatsby.Analysis.Syntax.Tree;
+
+namespace Gatsby.Analysis
+{
+    public class Compilation
+    {
+        public SyntaxTree Syntax { get;  }
+
+        public Compilation(SyntaxTree syntax)
+        {
+            Syntax = syntax;
+        }
+
+        public EvaluationResult Evaluate()
+        {
+            var binder = new SemanticBinder();
+            var boundExpression = binder.SemanticExpression(Syntax.Root);
+
+            var diagnostics = Syntax.Diagnostics.Concat(binder.Diagnostics).ToArray();
+
+            if (diagnostics.Any())
+                return new EvaluationResult(diagnostics,null);
+
+            var evaluator = new Evaluator(boundExpression);
+            var value = evaluator.Evaluate();
+            
+            return new EvaluationResult(Array.Empty<string>(),value);
+        }
+    }
+}
---
+++ Gatsby/Analysis/EvaluationResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gatsby.Analysis
+{
+    public sealed class EvaluationResult
+    {
+        public IReadOnlyList<string> Diagnostics { get; }
+        public object Value { get; }
+
+        public EvaluationResult(IEnumerable<string> diagnostics, object value)
+        {
+            Diagnostics = diagnostics.ToArray();
+            Value = value;
+        }
+    }
+}
